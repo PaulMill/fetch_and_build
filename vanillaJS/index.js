17ed85a3c21103196b82e745963c40b7a9046d07
PaulM <usa.paul@icloud.com>
2023-02-12 00:37:46 -0500

    Adds javascript implementation

--- vanillaJS/index.js
+++ vanillaJS/index.js
@@ -2,4 +2,55 @@
     // API endpoint url https://jsonplaceholder.typicode.com/albums/1/photos 
     // It need to show on page thumbnails with photos, but on photo click it need to redirect to show full size of photo at new tab
     // each photo should contain title on the top of thumbnail
+
+    // constants
+    const apiUrl = 'https://jsonplaceholder.typicode.com/albums/1/photos';
+    const textNoPhotos = 'User does not have any photos';
+    // existing DOM elemnts
+    const photoListRoot = document.querySelector('#photo-list_root');
+    const buttonSubmitEl = document.querySelector('#fetch-data-btn');
+
+
+    const addDataToDom = (list) => {
+        if(!list || list.length === 0) {
+            const pElement = document.createElement('p');
+            photoListRoot.append(textNoPhotos, pElement);
+            return;
+        }
+        const ul = document.createElement('ul');
+
+        list.forEach(({ title, thumbnailUrl, url }) => {
+            // create elements
+            const li = document.createElement('li');
+            const img = document.createElement('img');
+            const p = document.createElement('p');
+            const a = document.createElement('a');
+
+            img.setAttribute('src', thumbnailUrl);
+            img.setAttribute('alt', title);
+            a.setAttribute('href', url);
+            a.setAttribute('target', '_blank');
+            
+            a.append(p, title);
+            a.append(img);
+            li.append(a);
+            ul.append(li);
+        })
+        photoListRoot.append(ul);
+    }
+
+    const fetchData = async () => {
+        const response = await fetch(apiUrl);
+
+        if(response.ok) {
+            const data = await response.json();
+            addDataToDom(data);
+        } else {
+            console.error(new Error('Error occured on fetching data', { cause: response.status }))
+        }
+    }
+    // event listeners
+    buttonSubmitEl.addEventListener('click', fetchData);
+
+
 })();
